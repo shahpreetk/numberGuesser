6237ca64f63a1c4e91290189dbc6063105a98d73
Preet Shah <preetshah@Preets-MacBook-Pro.local>
2019-08-12 00:33:46 +0530

    JS Update 1 - win

---
+++ app.js
@@ -0,0 +1,32 @@
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message');
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+guessBtn.addEventListener('click', function () {
+    let guess = parseInt(guessInput.value);
+    if (isNaN(guess) || guess < min || guess > max) {
+        setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+    }
+
+    if (guess === winningNum){
+        guessInput.disabled = true;
+        guessInput.style.borderColor = 'green';
+        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+    }
+});
+
+function setMessage(msg, color) {
+    message.style.color = color;
+    message.textContent = msg;
+}

79db04f88311262972e8b37b4c252558edbbaf3b
Preet Shah <preetshah@Preets-MacBook-Pro.local>
2019-08-12 10:49:59 +0530

    Update 2 - lose

--- app.js
+++ app.js
@@ -19,12 +19,28 @@ guessBtn.addEventListener('click', function () {
         setMessage(`Please enter a number between ${min} and ${max}`, 'red');
     }
 
-    if (guess === winningNum){
-        guessInput.disabled = true;
-        guessInput.style.borderColor = 'green';
-        setMessage(`${winningNum} is correct, YOU WIN!`, 'green');
+    if (guess === winningNum) {
+        gameOver(true, `${winningNum} is correct, YOU WIN!`);
+    } else {
+        guessesLeft -= 1;
+        if (guessesLeft === 0) {
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+        } else {
+            guessInput.style.borderColor = 'red';
+            guessInput.value = '';
+            setMessage(`${guess} is not correct. ${guessesLeft} guesses left`, 'red');
+        }
     }
 });
+
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    setMessage(msg);
+}
 
 function setMessage(msg, color) {
     message.style.color = color;
